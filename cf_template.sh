a9fc718f303ec6db1e0ca9e475fca95a8a5ca7a6
Razan <razan.askar@awacademy.training>
2022-07-04 16:26:37 +0300

    ALB loadbalancer deployment successful

--- cf_template.sh
+++ cf_template.sh
@@ -5,3 +5,6 @@ aws cloudformation validate-template --template-body file://s3.yaml
 #Deploy changes to stack
 aws cloudformation deploy --template-file s3.yaml --stack-name razanstack
 #Update Stack
+aws cloudformation update-stack --stack-name razanstack --template-body file://s3.yaml
+#Describe Stack
+aws cloudformation describe-stacks --stack-name razanstack
--- links.sh
+++ links.sh
@@ -1,6 +1,12 @@
+#Amazon Cloud formation template snippets
+https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/CHAP_TemplateQuickRef.html
 #Amazon SQS template snippets
 https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/scenario-sqs-queue.html
 #Deploy changes to template
 https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-cli-deploy.html
 #Amazon ec2 template snippets
 https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/quickref-ec2.html
+#AWS::ElasticLoadBalancingV2::LoadBalancer
+https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-elasticloadbalancingv2-loadbalancer.html#cfn-elasticloadbalancingv2-loadbalancer-ipaddresstype
+#Additional link for ALB:
+https://dev.classmethod.jp/articles/cloudformation-template-for-vpc-with-ec2-and-alb/
